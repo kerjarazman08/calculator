ddb533d7c5fb8bb67a8467d91183bb902c1fc2f8
kerjarazman08 <kerjarazman08@gmail.com>
2025-07-30 14:48:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function clearDisplay() {
   document.getElementById('display').value = '';
 }
 
+function backspace() {
+  let display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
 function calculate() {
   try {
     const result = eval(document.getElementById('display').value);
